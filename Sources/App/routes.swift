9b3b5b1ed4ce0d6d6364839e171ef5762a7e275a
clrminded <the_rocket_scientist@icloud.com>
2024-09-12 14:02:12 -0700

    set up hello route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,34 @@
 import Vapor
+import SwiftHtml
 
 func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.routes.get("hello") { req async -> Response in
+        let doc = Document(.html) {
+            Html
+            {
+                Head
+                {
+                    Title("Hello World!")
+                }
+                Body
+                {
+                    H1("Hello, World!")
+                }
+            }
+        }
+        
+        let body = DocumentRenderer(minify: false, indent: 4).render(doc)
+        
+        return Response(
+            status: .ok,
+            headers: [
+                "Content-Type": "text/html; charset=utf-8"
+            ],
+            body: .init(string: body)
+        )
     }
 }
