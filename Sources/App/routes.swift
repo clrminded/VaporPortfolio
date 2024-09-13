9475ac0bd4e89a3fce69adf4757dd60cec81b782
clrminded <the_rocket_scientist@icloud.com>
2024-09-12 22:12:59 -0700

    updated route using new syntax

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,34 +1,47 @@
 import Vapor
 import SwiftHtml
 
+/*
+ The MyTemplate struct conforms to the TemplateRepresentable
+ protocol. Then we introduce a contextual variable called title.
+ We can pass the context to our template when we initialize it,
+ and we can access the title in the render method.
+ 
+ since we added the extension to Response we can now call
+ req.templates.renderHtml() to return an HTML response.
+ */
+
+struct MyTemplate: TemplateRepresentable {
+    
+    let title: String
+    
+    func render(_ req: Request) -> Tag 
+    {
+        Html
+        {
+            Head
+            {
+                Title(title)
+            }
+            
+            Body
+            {
+                H1(title)
+            }
+        }
+    }
+}
+
 func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
 
     app.routes.get("hello") { req async -> Response in
-        let doc = Document(.html) {
-            Html
-            {
-                Head
-                {
-                    Title("Hello World!")
-                }
-                Body
-                {
-                    H1("Hello, World!")
-                }
-            }
-        }
         
-        let body = DocumentRenderer(minify: false, indent: 4).render(doc)
-        
-        return Response(
-            status: .ok,
-            headers: [
-                "Content-Type": "text/html; charset=utf-8"
-            ],
-            body: .init(string: body)
+        req.templates.renderHtml(
+            MyTemplate(title: "Hello World!")
         )
+        
     }
 }
