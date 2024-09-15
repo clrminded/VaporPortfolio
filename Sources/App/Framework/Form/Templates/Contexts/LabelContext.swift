2ae6ebd971903ec169f7ba776c28828a47b02371
clrminded <the_rocket_scientist@icloud.com>
2024-09-14 18:13:52 -0700

    added LabelContext and LabelTemplate to render a form field object

---
+++ Sources/App/Framework/Form/Templates/Contexts/LabelContext.swift
@@ -0,0 +1,25 @@
+//
+//  LabelContext.swift
+//
+//
+//  This will represent a label for a given form field
+
+public struct LabelContext
+{
+    public var key: String
+    public var title: String?
+    public var required: Bool
+    public var more: String?
+    
+    public init(
+        key: String,
+        title: String? = nil,
+        required: Bool = false,
+        more: String? = nil
+    ) {
+        self.key = key
+        self.title = title
+        self.required = required
+        self.more = more
+    }
+}
