bdb71277a90d3d2ed8ad6244c0f31a663b778ee3
clrminded <the_rocket_scientist@icloud.com>
2024-09-14 18:29:31 -0700

    added InputFieldContext and InputFieldTemplate

---
+++ Sources/App/Framework/Form/Fields/InputFieldTemplate.swift
@@ -0,0 +1,33 @@
+//
+//  InputFieldTemplate.swift
+//
+//
+
+
+import Vapor
+import SwiftHtml
+
+public struct InputFieldTemplate: TemplateRepresentable 
+{
+    public var context: InputFieldContext
+    public init(_ context: InputFieldContext) {
+        self.context = context
+    }
+    
+    @TagBuilder
+    public func render(_ req: Request) -> Tag
+    {        
+        LabelTemplate(context.label).render(req)
+        Input()
+            .type(context.type)
+            .key(context.key)
+            .placeholder(context.placeholder)
+            .value(context.value)
+            .class("field")
+        
+        if let error = context.error {
+            Span(error)
+                .class("error")
+        }
+    }
+}
