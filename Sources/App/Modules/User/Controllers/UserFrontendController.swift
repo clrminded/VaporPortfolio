806023ce2bf421c912c932f052acfe64c0e3f0f8
clrminded <the_rocket_scientist@icloud.com>
2024-09-15 00:33:27 -0700

    refactored user login

--- Sources/App/Modules/User/Controllers/UserFrontendController.swift
+++ Sources/App/Modules/User/Controllers/UserFrontendController.swift
@@ -10,23 +10,18 @@ import Vapor
 
 struct UserFrontendController
 {
-    private struct Input: Decodable
-    {
-        let email: String?
-        let password: String?
-    }
-    
+
     private func renderSignInView(
         _ req: Request,
-        _ input: Input? = nil,
-        _ error: String? = nil
+        _ form: UserLoginForm
     ) -> Response
     {
         let template = UserLoginTemplate(
             .init(
                 icon: "⬇️",
                 title: "Sign in",
-                message: "Please log in with your existing account"
+                message: "Please log in with your existing account",
+                form: form.render(req: req)
             )
         )
         return req.templates.renderHtml(template)
@@ -35,7 +30,7 @@ struct UserFrontendController
     
     func signInView(_ req: Request) async throws -> Response
     {
-        renderSignInView(req)
+        renderSignInView(req, .init())
     }
     
     func signInAction(_ req: Request) async throws -> Response
@@ -46,13 +41,10 @@ struct UserFrontendController
             req.session.authenticate(user)
             return req.redirect(to: "/")
         }
-        /// if the user credentials were wrong we render the form again with an error
-        let input = try req.content.decode(Input.self)
-        return renderSignInView(
-            req,
-            input,
-            "Invalid email or password"
-        )
+        let form = UserLoginForm()
+        try await form.process(req: req)
+        form.error = "Invalid email or password."
+        return renderSignInView(req,form)
     }
     
     func signOut(req: Request) throws -> Response
--- Sources/App/Modules/User/Templates/Contexts/UserLoginTemplate.swift
+++ Sources/App/Modules/User/Templates/Contexts/UserLoginTemplate.swift
@@ -31,51 +31,8 @@ struct UserLoginTemplate: TemplateRepresentable
                 }
                 .class("lead")
                 
-                Form
-                {
-                    // if there is an error assign it to variable
-                    // error and run code block
-                    if let error = context.error
-                    {
-                        Section
-                        {
-                            Span(error)
-                                .class("error")
-                        }
-                    }
-                    // when no errors occur continue running
-                    Section
-                    {
-                        Label("Email")
-                            .for("email")
-                        Input()
-                            .key("email")
-                            .type(.email)
-                            .value(context.email)
-                            .class("field")
-                    }
-                    
-                    Section
-                    {
-                        Label("Password")
-                            .for("password")
-                        Input()
-                            .key("password")
-                            .type(.password)
-                            .value(context.password)
-                            .class("field")
-                    }
-                    
-                    Section
-                    {
-                        Input()
-                            .type(.submit)
-                            .value("Sign In")
-                            .class("submit")
-                    }
-                }
-                .action("/sign-in/")
-                .method(.post)
+                context.form.render(req)
+                
             }
             .id("user-login")
             .class("container")
