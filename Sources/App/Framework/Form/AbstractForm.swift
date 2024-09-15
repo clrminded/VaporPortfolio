23608b54ba04902945fd07e74e48c5d68cb820ca
clrminded <the_rocket_scientist@icloud.com>
2024-09-15 00:16:51 -0700

    set up to get ready to refactor userlogin

--- Sources/App/Framework/Form/AbstractForm.swift
+++ Sources/App/Framework/Form/AbstractForm.swift
@@ -8,16 +8,16 @@
 
 import Vapor
 
-open class AbstractForm
+open class AbstractForm: FormComponent
 {
     open var action: FormAction
-    open var fields: [Any]
+    open var fields: [FormComponent]
     open var error: String?
     open var submit: String?
     
     public init(
         action: FormAction = .init(),
-        fields: [Any] = [],
+        fields: [FormComponent] = [],
         error: String? = nil,
         submit: String? = nil
     ) {
@@ -27,5 +27,54 @@ open class AbstractForm
         self.submit = submit
         
         self.action.enctype = .multipart
+    }
+    
+    open func load(req: Vapor.Request) async throws {
+         for field in fields {
+             try await field.load(req: req)
+         }
+    }
+    
+    open func process(req: Vapor.Request) async throws {
+        for field in fields {
+            try await field.process(req: req)
+        }
+    }
+    
+    open func validate(req: Vapor.Request) async throws -> Bool {
+        var result: [Bool] = []
+        for field in fields {
+            result.append(try await field.validate(req: req))
+        }
+        return result.filter { $0 == false }.isEmpty
+    }
+    
+    open func write(req: Vapor.Request) async throws {
+        for field in fields {
+            try await field.write(req: req)
+        }
+    }
+    
+    open func save(req: Vapor.Request) async throws {
+        for field in fields {
+            try await field.save(req: req)
+        }
+    }
+    
+    open func read(req: Vapor.Request) async throws {
+        for field in fields {
+            try await field.read(req: req)
+        }
+    }
+    
+    open func render(req: Vapor.Request) -> TemplateRepresentable {
+        FormTemplate(
+            .init(
+                action: action,
+                fields: fields.map { $0.render(req: req) },
+                error: error,
+                submit: submit
+            )
+        )
     }
 }
--- Sources/App/Framework/Form/AbstractFormField.swift
+++ Sources/App/Framework/Form/AbstractFormField.swift
@@ -8,7 +8,7 @@ import Vapor
 
 open class AbstractFormField<
     Input: Decodable,
-    Output: TemplateRepresentable>
+    Output: TemplateRepresentable>: FormComponent
 {
     public var key: String
     public var input: Input
@@ -33,5 +33,35 @@ open class AbstractFormField<
     {
         block(self)
         return self
+    }
+    
+    open func load(req: Vapor.Request) async throws {
+         
+    }
+    
+    open func process(req: Vapor.Request) async throws {
+        if let value = try? req.content.get(Input.self, at: key) {
+            input = value
+        }
+    }
+    
+    open func validate(req: Vapor.Request) async throws -> Bool {
+        true
+    }
+    
+    open func write(req: Vapor.Request) async throws {
+        
+    }
+    
+    open func save(req: Vapor.Request) async throws {
+        
+    }
+    
+    open func read(req: Vapor.Request) async throws {
+        
+    }
+    
+    open func render(req: Vapor.Request) -> TemplateRepresentable {
+        output
     }
 }
