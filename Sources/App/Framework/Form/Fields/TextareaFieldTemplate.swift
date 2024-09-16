1f54c1fb6b17a8c290a5b7608354adc4f579bb70
clrminded <the_rocket_scientist@icloud.com>
2024-09-15 21:18:31 -0700

    created Textarea fields

---
+++ Sources/App/Framework/Form/Fields/TextareaFieldTemplate.swift
@@ -0,0 +1,31 @@
+//
+//  TextareaFieldTemplate.swift
+//
+//
+
+import Vapor
+import SwiftHtml
+
+public struct TextareaFieldTemplate: TemplateRepresentable
+{
+    public var context: TextareaFieldContext
+    
+    public init(_ context: TextareaFieldContext) {
+        self.context = context
+    }
+    
+    @TagBuilder
+    public func render(_ req: Request) -> Tag 
+    {
+        LabelTemplate(context.label).render(req)
+        
+        Textarea(context.value)
+            .placeholder(context.placeholder)
+            .name(context.key)
+        
+        if let error = context.error {
+            Span(error)
+                .class("error")
+        }
+    }
+}
