21b6354cf861271f8012b5720dde61b8170a49a7
clrminded <the_rocket_scientist@icloud.com>
2024-09-16 23:15:10 -0700

    added Liquid to project and updated configure

--- Package.swift
+++ Package.swift
@@ -20,6 +20,14 @@ let package = Package(
             from: "4.1.0"
         ),
         .package(
+            url: "https://github.com/binarybirds/liquid",
+            from: "1.3.0"
+        ),
+        .package(
+            url: "https://github.com/binarybirds/liquid-local-driver",
+            from: "1.3.0"
+        ),
+        .package(
             url: "https://github.com/binarybirds/swift-html",
             from: "1.7.0"
         ),
@@ -29,6 +37,8 @@ let package = Package(
             .product(name: "Vapor", package: "vapor"),
             .product(name: "Fluent", package: "fluent"),
             .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+            .product(name: "Liquid", package: "liquid"),
+            .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
             .product(name: "SwiftHtml", package: "swift-html"),
             .product(name: "SwiftSvg", package: "swift-html"),
         ]),
