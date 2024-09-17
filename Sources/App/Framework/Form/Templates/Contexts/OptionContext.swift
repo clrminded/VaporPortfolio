13b7a95858190eec1317d4fe5fed9c331741d573
clrminded <the_rocket_scientist@icloud.com>
2024-09-16 22:49:56 -0700

    added optioncontext struct

---
+++ Sources/App/Framework/Form/Templates/Contexts/OptionContext.swift
@@ -0,0 +1,18 @@
+//
+//  OptionContext.swift
+//
+//
+//  Select Field
+
+public struct OptionContext
+{
+    public var key: String
+    public var label: String
+    
+    public init(key: String, label: String) {
+        self.key = key
+        self.label = label
+    }
+}
+
+
